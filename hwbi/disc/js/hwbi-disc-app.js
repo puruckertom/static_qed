2b2860b89302dd33f62d6b4bfa0f88c1a288623b
Connor <connor.thorson@gmail.com>
2018-08-10 11:55:37 -0700

    re-worked main search bar, removed search button, added ability to type partial address and press enter to search for first result

--- hwbi/disc/js/hwbi-disc-app.js
+++ hwbi/disc/js/hwbi-disc-app.js
@@ -252,6 +252,42 @@ function initializeAutocomplete() {
     });
     // searchBox.setComponentRestrictions({'country': ['us']});
     searchBox.addListener('place_changed', setLocationValue);
+
+    var pac_input = document.getElementById('search_field');
+
+    (function pacSelectFirst(input) {
+        // store the original event binding function
+        var _addEventListener = (input.addEventListener) ? input.addEventListener : input.attachEvent;
+
+        function addEventListenerWrapper(type, listener) {
+            // Simulate a 'down arrow' keypress on hitting 'return' when no pac suggestion is selected,
+            // and then trigger the original listener.
+            if (type == "keydown") {
+                var orig_listener = listener;
+                listener = function(event) {
+                    var suggestion_selected = $(".pac-item-selected").length > 0;
+                    if (event.keyCode == 13 && !suggestion_selected) {
+                        var simulated_downarrow = $.Event("keydown", {
+                            keyCode: 40,
+                            which: 40
+                        });
+                        orig_listener.apply(input, [simulated_downarrow]);
+                    }
+
+                    orig_listener.apply(input, [event]);
+                };
+            }
+
+            _addEventListener.apply(input, [type, listener]);
+        }
+
+        input.addEventListener = addEventListenerWrapper;
+        input.attachEvent = addEventListenerWrapper;
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+
+    })(pac_input);
+
 }
 
 function setLocationValue() {
@@ -283,6 +319,8 @@ function setLocationValue() {
     json_value["state"] = state;
     json_value["state_abbr"] = stateAbbr;
     locationValue = JSON.stringify(json_value);
+
+    getScoreData();
 }
 
 function setCookie(cname, cvalue, exdays) {
