95bb86eb35eaa558211c96f0defd6b4f2d1196b0
Connor <connor.thorson@gmail.com>
2018-12-19 10:06:19 -0800

    added functions to show (certain) specific sliders based on which slinky menu item selected

--- hwbi/disc/js/main.js
+++ hwbi/disc/js/main.js
@@ -167,3 +167,38 @@ function tabsOnClick(id){
 }
 
 
+filterSelection('all')
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName('filterDiv');
+    if (c == 'all') c = '';
+    for (i = 0; i < x.length; i++) {
+        removeShow(x[i], 'show');
+        if (x[i].className.indexOf(c) > -1) addShow(x[i], 'show');
+    }
+}
+
+function addShow(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(' ');
+    arr2 = name.split(' ');
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += ' ' + arr2[i];
+        }
+    }
+}
+
+function removeShow(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1); 
+      }
+    }
+    element.className = arr1.join(" ");
+  }
+
+
