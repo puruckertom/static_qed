ffc8eccbee1da4401b6974681387d75887432efa
Connor <connor.thorson@gmail.com>
2019-04-05 15:40:03 -0700

    re-applied a fix for the scrolling issue when clicking on the first snapshot accordion item

--- hwbi/disc/js/hwbi-disc-app.js
+++ hwbi/disc/js/hwbi-disc-app.js
@@ -451,12 +451,14 @@ function setAccordion() {
                 } else {
                     panel.style.display = "block";
                     $(this).addClass("active");
-                    $('html, body').animate({
-                        scrollTop: $(this).offset().top
+                    if(!$(this).parent().is('#nature-block')) {
+                        $('html, body').animate({
+                        scrollTop: $('.domain-score-block').offset().top
                     }, 300);
                 }
-            }
-        );
+                }
+            });
+
     }
 }
 
