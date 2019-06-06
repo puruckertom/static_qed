18a2e8cf5d9d8495828abf343e4eb0584fe80807
Connor <connor.thorson@gmail.com>
2019-06-06 15:22:42 -0700

    changed location of buttons on scenario builder page and fixed size/alignment of aster plot

--- hwbi/disc/js/draw.js
+++ hwbi/disc/js/draw.js
@@ -1,7 +1,7 @@
 var rivData;
 var drawn = false;
-var width = 275,
-    height = 275,
+var width = 300,
+    height = 300,
     radius = Math.min(width, height) / 2,
     innerRadius = 0.3 * radius;
 
